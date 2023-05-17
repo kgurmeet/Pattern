261c477e2662c888955a9c4512fdc2a13bf43b7a
kgurmeet <131999481+kgurmeet@users.noreply.github.com>
2023-05-18 00:02:05 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+public class Pattern{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                if((i+j)%2==0)
+                System.out.print("1");
+                
+                else
+                System.out.print("0");
+                
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
